09983dcfe475d2b9019e455e91db94e5e79b4e56
Marianna Zhukova <mzhukova@c1r2s6.42wolfsburg.de>
2024-04-09 15:42:01 +0200

    makefile setup

--- main.c
+++ main.c
@@ -6,18 +6,23 @@
 /*   By: mzhukova <mzhukova@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/14 13:45:58 by mariannazhu       #+#    #+#             */
-/*   Updated: 2024/04/09 11:47:14 by mzhukova         ###   ########.fr       */
+/*   Updated: 2024/04/09 15:39:45 by mzhukova         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
-#include <mlx.h>
 
-int	main(void)
+int	main(int argc, char **argv)
 {
 	void	*mlx;
 	void	*mlx_win;
+	int		map;
 
+	if (argc != 2)
+		return(ft_printf("Incorrect amount of arguments!\n"));
+	if (ft_strnstr(argv[1], ".ber", 4) != NULL)
+		return(ft_printf("Incorrect map name!\n"));
+	map = open(argv[1], O_RDONLY, 0644);
 	mlx = mlx_init();
 	mlx_win = mlx_new_window(mlx, 1920, 1080, "Hello world!");
 	mlx_loop(mlx);
--- so_long.h
+++ so_long.h
@@ -3,15 +3,20 @@
 /*                                                        :::      ::::::::   */
 /*   so_long.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mariannazhukova <mariannazhukova@studen    +#+  +:+       +#+        */
+/*   By: mzhukova <mzhukova@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/14 13:45:27 by mariannazhu       #+#    #+#             */
-/*   Updated: 2024/03/14 14:02:55 by mariannazhu      ###   ########.fr       */
+/*   Updated: 2024/04/09 13:17:29 by mzhukova         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SO_LONG_H
 
 #define SO_LONG_H
+
+# include <mlx.h>
+# include <fcntl.h>
+# include "./libft/libft.h"
+# include "./libft/printf/ft_printf.h"
 
 #endif
