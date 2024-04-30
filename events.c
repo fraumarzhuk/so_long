607262eda90ed5e8b0ebea2b69644059091908b7
Marianna Zhukova <mzhukova@c1r2s6.42wolfsburg.de>
2024-04-30 12:25:32 +0200

    refactor events and norm

--- events.c
+++ events.c
@@ -1,24 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   events.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mzhukova <mzhukova@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/30 12:22:38 by mzhukova          #+#    #+#             */
+/*   Updated: 2024/04/30 12:24:13 by mzhukova         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "so_long.h"
 
-int key_press(int keycode, t_env *env)
+int	key_press(int keycode, t_env *env)
 {
-	// ESC key to exit
 	if (keycode == 53)
 	{
+		/*add free everything*/
 		mlx_destroy_window(env->mlx, env->mlx_win);
 		exit(1);
 	}
-	// move right
 	if (keycode == 124 || keycode == 2)
 		move_on_x(env, env->args->player_y, env->args->player_x + 1, 1);
-	//move left
 	if (keycode == 123 || keycode == 0)
-		move_on_x(env, env->args->player_y, env->args->player_x - 1, -1);
-	//move_up
+		move_on_x(env, env->args->player_y, env->args->player_x -1, -1);
 	if (keycode == 126 || keycode == 13)
 		move_on_y(env, env->args->player_y - 1, env->args->player_x, -1);
-	//move_down
 	if (keycode == 125 || keycode == 1)
 		move_on_y(env, env->args->player_y + 1, env->args->player_x, 1);
 	return (0);
@@ -35,9 +42,11 @@ void	move_on_y(t_env *env, int y, int x, int sign)
 		printf("Moves: %i\n", env->args->moves);
 	}
 }
+
 void	move_on_x(t_env *env, int y, int x, int sign)
 {
-	if (env->args->map[y][x] != '1' && (env->args->map[y][x]) && env->args->map[y][x] != '\n')
+	if (env->args->map[y][x] != '1' && (env->args->map[y][x])
+		&& env->args->map[y][x] != '\n')
 	{
 		env->args->player_x += sign;
 		check_collects(env);
