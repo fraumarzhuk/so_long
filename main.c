17ec4a4b062042dd78638af6a37b235e86c94b10
Marianna Zhukova <mzhukova@c1r2s6.42wolfsburg.de>
2024-04-30 17:14:33 +0200

    cleaned main a bit

--- main.c
+++ main.c
@@ -6,12 +6,38 @@
 /*   By: mzhukova <mzhukova@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/14 13:45:58 by mariannazhu       #+#    #+#             */
-/*   Updated: 2024/04/30 16:03:31 by mzhukova         ###   ########.fr       */
+/*   Updated: 2024/04/30 17:10:15 by mzhukova         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
+void	check_args(t_args *args, char **argv)
+{
+	args->fd = open(argv[1], O_RDONLY);
+	if (args->fd < 0 || read(args->fd, 0, 0) < 0)
+		so_short_error("Error reading a file.", args, false);
+	if (!map_init(args))
+		so_short_error("Error initing a map.", args, true);
+	map_validation(argv[1], args);
+}
+
+void	init_img(t_data *img)
+{
+	img->height = 1080;
+	img->width = 1920;
+	img->player_pic_path = "./img/player/player_1.xpm";
+	img->is_player_2 = false;
+	img->player_back = false;
+}
+
+void	init_args(t_args *args)
+{
+	args->line_len = ft_strlen(&args->map[0][0]) - 1;
+	args->collects_collected = false;
+	args->moves = 0;
+	args->frame = 0;
+}
 
 int	main(int argc, char **argv)
 {
@@ -19,36 +45,26 @@ int	main(int argc, char **argv)
 	void	*mlx_win;
 	t_args	*args;
 	t_data	*img;
-	
+	t_env	env;
+
 	args = malloc(sizeof(t_args));
 	if (!args)
-    	so_short_error("Malloc failed!", args, false);
+		so_short_error("Malloc failed!", args, false);
 	if (argc != 2)
 		so_short_error("Incorrect amount of arguments.", args, false);
-	args->fd = open(argv[1], O_RDONLY);
-	if (args->fd < 0 || read(args->fd, 0, 0) < 0)
-		so_short_error("Error reading a file.", args, false);
-	if (!map_init(args))
-		so_short_error("Error initing a map.", args, true);
-	map_validation(argv[1], args);
+	check_args(args, argv);
+	init_args(args);
 	mlx = mlx_init();
-	args->line_len = ft_strlen(&args->map[0][0]) - 1;
-	mlx_win = mlx_new_window(mlx, args->line_len * 65, args->line_count * 65, "So long!");
+	mlx_win = mlx_new_window(mlx, args->line_len * 65,
+			args->line_count * 65, "So long!");
 	img = malloc(sizeof(t_data));
 	if (!img)
 		so_short_error("Malloc failed!", args, true);
-	img->height = 1080;
-	img->width = 1920;
-	args->collects_collected = false;
+	init_img(img);
 	render_everything(img, mlx, mlx_win, args);
-	args->moves = 0;
-	args->frame = 0;
-	img->player_pic_path = "./img/player/player_1.xpm";
-	img->is_player_2 = false;
-	img->player_back = false;
-	t_env env = {args, img, mlx, mlx_win};
+	env = (t_env){args, img, mlx, mlx_win};
 	mlx_loop_hook(mlx, frameupdate, &env);
 	mlx_key_hook(mlx_win, key_press, &env);
 	mlx_loop(mlx);
-	return(0);
+	return (0);
 }
