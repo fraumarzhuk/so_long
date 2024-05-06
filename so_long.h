75228606795de890d1804c665f226d82cc76c585
Marianna Zhukova <mzhukova@c2r12s8.42wolfsburg.de>
2024-05-06 13:14:00 +0200

    done

--- so_long.h
+++ so_long.h
@@ -6,7 +6,7 @@
 /*   By: mzhukova <mzhukova@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/14 13:45:27 by mariannazhu       #+#    #+#             */
-/*   Updated: 2024/05/04 17:47:07 by mzhukova         ###   ########.fr       */
+/*   Updated: 2024/05/06 13:08:51 by mzhukova         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,6 +53,8 @@ typedef struct s_data
 	bool	player_back;
 	bool	is_player_2;
 	bool	player_exit;
+	int		img_width;
+	int		img_height;
 }				t_data;
 
 // game itself, important info about 
--- src/rendering.c
+++ src/rendering.c
@@ -6,7 +6,7 @@
 /*   By: mzhukova <mzhukova@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/16 15:35:09 by mzhukova          #+#    #+#             */
-/*   Updated: 2024/05/04 17:15:12 by mzhukova         ###   ########.fr       */
+/*   Updated: 2024/05/06 13:12:08 by mzhukova         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,18 +44,16 @@ void	render_walls(t_data *img, void	*mlx, void *mlx_win, t_args *args)
 void	render_background(t_data *img, void	*mlx, void *mlx_win, t_args *args)
 {
 	char	*relative_path;
-	int		img_width;
-	int		img_height;
 	int		x;
 	int		y;
 
-	img_width = 65;
-	img_height = 65;
+	img->img_width = 65;
+	img->img_height = 65;
 	x = 0;
 	y = 0;
 	relative_path = "./img/bg.xpm";
 	img->img = mlx_xpm_file_to_image(mlx, relative_path,
-			&img_width, &img_height);
+			&img->img_width, &img->img_height);
 	while (y < args->line_count)
 	{
 		while (args->map[y][x] != '\n' && args->map[y][x])
@@ -103,19 +101,18 @@ void	render_collects(t_data *img, void	*mlx, void *mlx_win, t_args *args)
 void	render_exit(t_data *img, void	*mlx, void *mlx_win, t_args *args)
 {
 	char	*img_path;
-	int		img_width;
-	int		img_height;
 	int		x;
 	int		y;
 
-	img_width = 65;
-	img_height = 65;
+	img->img_width = 65;
+	img->img_height = 65;
 	x = 0;
 	y = 0;
 	img_path = "./img/door_closed.xpm";
 	if (args->collects_collected == true)
 		img_path = "./img/door_open.xpm";
-	img->img = mlx_xpm_file_to_image(mlx, img_path, &img_width, &img_height);
+	img->img = mlx_xpm_file_to_image(mlx, img_path,
+			&img->img_width, &img->img_height);
 	while (y < args->line_count)
 	{
 		while (args->map[y][x] != '\n' && args->map[y][x])
